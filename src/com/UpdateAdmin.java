a3415ff20849b85f014dedb84c8663998f16467b
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2021-01-10 17:46:35 +0000

    Admin page has functional clear button

--- src/com/UpdateAdmin.java
+++ src/com/UpdateAdmin.java
@@ -1,10 +1,35 @@
 package com;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class UpdateAdmin {
-    private JTextField textField1;
-    private JTextField textField2;
+public class UpdateAdmin extends JFrame {
+    private JTextField adminName;
+    private JTextField adminPass;
     private JButton updateButton;
     private JButton clearButton;
+    private JPanel mainPanel;
+
+    public static void main(String[] args) {
+        UpdateAdmin page = new UpdateAdmin();
+        page.setVisible(true);
+    }
+
+    public UpdateAdmin () {
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(500, 600));
+        pack();
+
+        clearButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                adminName.setText("");
+                adminPass.setText("");
+            }
+        });
+    }
 }
